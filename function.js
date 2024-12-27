229471662407318d4a914cea25492ebdfe23b8ed
VishalSrivastava-007 <vishal.srivastava1@sourcefuse.com>
2024-12-27 11:33:46 +0530

    added javascipt

---
+++ function.js
@@ -0,0 +1,26 @@
+// 1. Function Declaration  -> Defines a named function.
+// function add(a, b) {
+//     return a + b;
+// }
+// console.log(add(3, 4)); // 7
+
+// 2. Function Expression -> Defines an anonymous function and assigns it to a variable.
+// const multiply = function(a, b) {
+//     return a * b;
+// };
+// console.log(multiply(3, 4)); // 12
+
+// 3.  Arrow Functions -> arrow functions allow for a more concise and readable code
+// const subtract = (a, b) => a - b;
+// console.log(subtract(5, 3)); // 2
+
+// 4. IIFE (Immediately Invoked Function Expression) -> Executes immediately after definition.
+// (function() {
+//     console.log("IIFE executed");
+// })();
+
+// 5. Parameters and Default Values -> Functions can have default parameter values.
+// function greet(name = "Guest") {
+//     console.log(`Hello, ${name}!`);
+// }
+// greet(); // Hello, Guest!
