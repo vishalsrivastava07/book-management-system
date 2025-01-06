ba968a06eebd69036999c8d10e5dcf4b57bc9baa
VishalSrivastava-007 <vishal.srivastava1@sourcefuse.com>
2025-01-06 15:44:52 +0530

    Applied const Keyword

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 // Load books from localStorage and simulated server
-// Load books from localStorage and simulated server
 async function loadBooks(filter = "", sortOption = "") {
     const bookList = document.getElementById("book-list");
     bookList.innerHTML = "";
