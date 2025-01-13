f7f41085436f915972ef34e9ea481807954efd5e
VishalSrivastava-007 <vishal.srivastava1@sourcefuse.com>
2025-01-13 11:33:37 +0530

    Implemented inheritance

--- script.js
+++ script.js
@@ -42,6 +42,47 @@ class BaseManager {
     }
 }
 
+
+
+// BaseBook class
+class BaseBook {
+    constructor({ title, author, price }) {
+        this.title = title;
+        this.author = author;
+        this.price = price;
+    }
+
+    getBookDetails() {
+        return `Title: ${this.title}, Author: ${this.author}, Price: $${this.price}`;
+    }
+}
+
+// PrintedBook class extending BaseBook
+class PrintedBook extends BaseBook {
+    constructor({ title, author, price, bookCondition, dimensions, coverType }) {
+        super({ title, author, price });
+        this.bookCondition = bookCondition;
+        this.dimensions = dimensions;
+        this.coverType = coverType;
+    }
+
+    getPrintedBookDetails() {
+        return `Printed Book - ${this.getBookDetails()}, Condition: ${this.bookCondition}, Dimensions: ${this.dimensions}, Cover Type: ${this.coverType}`;
+    }
+}
+
+// EBook class extending BaseBook
+class EBook extends BaseBook {
+    constructor({ title, author, price, fileSize }) {
+        super({ title, author, price });
+        this.fileSize = fileSize;
+    }
+
+    getEBookDetails() {
+        return `EBook - ${this.getBookDetails()}, File Size: ${this.fileSize} MB`;
+    }
+}
+
 class BookManager extends BaseManager {
     constructor() {
         super();
